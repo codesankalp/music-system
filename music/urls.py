813dac21d983488e172f0bdbc884ed8aad78e3c8
sankalp <sankalp123427@gmail.com>
2020-07-10 12:21:21 +0530

    adding follow hyperlink on main page

--- music/urls.py
+++ music/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('',views.index,name = "home"),
     path('add/song',views.add_song,name='add'),
     path('view/<str:playlist>',views.viewplay),
-    path('follow/',views.follow),
+    path('follow/',views.follow,name='follow'),
     path('playlist/create/',views.create_playlist,name="create"),
     path('playlist/edit/',views.edit,name='edit'),
     path('playlist/edit/transfer/<str:playlist>',views.transfer,name='transfer'),
